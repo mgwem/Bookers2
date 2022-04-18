864de8c567af4df789688df1a5f84b49170470c6
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-04-18 04:02:37 +0000

    [Update]users/editページ

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:index, :show, :edit, :create, :destroy]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
